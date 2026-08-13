7808b17351cebdf1795fe7bd20e6b41301dd3d38
Foksid <slavyan149@gmail.com>
2026-08-13 18:53:44 +0500

    думаю он не нужен, тем более поиск не полностью по нему работает

--- Xr_engine_orig/xrNetServer/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-#pragma comment(lib,	"xrCore"			)
-#pragma comment(lib,"depr/dxguid.lib")
-#pragma comment(lib,"Ws2_32.lib")
